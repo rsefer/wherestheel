aa1ec30f97310e32d57dfacfbcd45fc6a32a23fc
Robert Sefer <rsefer@gmail.com>
2016-12-12 11:37:34 -0600

    Added odds and ends

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/s/:id', to: 'pages#station'
 
+  get '/about', to: redirect('/', status: 301)
+
   get '/update_station_arrivals', to: 'pages#update_station_arrivals'
   get '/find_nearest_station', to: 'pages#find_nearest_station'
   get '/display_alerts', to: 'pages#display_alerts'
