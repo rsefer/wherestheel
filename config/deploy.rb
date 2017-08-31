693bc9f6e65ccef9d26ffdeb24825122b2e2d9aa
Robert Sefer <rsefer@gmail.com>
2017-08-31 13:05:59 -0500

    Added sake

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,14 @@ namespace :logs do
   end
 end
 
+namespace :sake do
+  desc "Run a task on a remote server."
+  # run like: cap staging rake:invoke task=a_certain_task
+  task :invoke do
+    run("cd #{deploy_to}/current && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+  end
+end
+
 # Change these
 server '104.131.172.33', port: 22, roles: [:web, :app, :db], primary: true
 
