e81a99bdf03ce57e1af04aef27140b3d188e4657
Robert Sefer <rsefer@gmail.com>
2017-08-31 13:08:03 -0500

    Fixed sake?

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,9 @@ end
 
 namespace :sake do
   desc "Run a task on a remote server."
-  # run like: cap staging rake:invoke task=a_certain_task
+  # run like: cap staging sake:invoke task=a_certain_task
   task :invoke do
-    run("cd #{deploy_to}/current && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+    run("cd #{deploy_to}/current && bundle exec rake #{ENV['task']} RAILS_ENV=#{fetch(:rails_env)}")
   end
 end
 
